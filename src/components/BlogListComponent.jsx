b74cce04aa535860a08079a7620698a26cf6ba7c
muhamademiralfani <muhamademiralfani@gmail.com>
2024-12-01 18:37:54 +0700

    refactor(component): refactor style img bloglistcomponent

--- src/components/BlogListComponent.jsx
+++ src/components/BlogListComponent.jsx
@@ -28,14 +28,14 @@ const BlogListComponent = () => {
     <>
       {blogs.map((post) => (
         <Link key={post.key} to={`/blog/${encodeURIComponent(post.key)}`} onClick={() => dispatch(fetchBlogDetail(post.key))} className='p-4 md:p-2 lg:p-0'>
-          <img src={post.thumb} alt={post.title} className='w-full lg:h-56 md:h-56 h-60 object-cover' />
+          <img src={post.thumb} alt={post.title} className='w-full lg:h-80 md:h-56 h-60 object-cover ' />
           <div className='flex flex-col gap-y-4'>
             <div className='flex items-center text-sm text-purple-600 mt-6'>
               <span>{post.author}</span>
               <span className='mx-2'>•</span>
               <span>{post.time}</span>
             </div>
-            <h3 className='text-xl font-semibold line-clamp-2'>{post.title}</h3>
+            <h3 className='text-xl font-semibold text-start line-clamp-1 hover:text-purple-600'>{post.title}</h3>
             <p className='text-sm text-gray-500 line-clamp-2'>{post.desc}</p>
             <div className='flex flex-wrap gap-2'>
               <span className={`px-3 py-1 rounded-full text-sm ${post.tag === 'Games' ? 'text-purple-700 bg-purple-100' : post.tag === 'News' ? 'text-blue-700 bg-blue-100' : 'text-pink-700 bg-pink-100'}`}>{post.tag}</span>
--- src/components/NewsLetterComponent.jsx
+++ src/components/NewsLetterComponent.jsx
@@ -22,22 +22,20 @@ const NewsLetterComponent = () => {
     <div className='flex flex-col gap-y-4 justify-center items-center mt-20 mb-20 text-center'>
       <span className='text-purple-600 text-base'>Newsletters</span>
       <h1 className='lg:text-5xl md:text-3xl text-2xl font-semibold '>Stories and interviews</h1>
-      <p className='text-[#667085] lg:text-xl md:text-base text-sm lg:leading-10 md:leading-6 leading-6'>
-        Subscribe to learn about new product features, the latest in technology, <br /> solutions, and updates.
-      </p>
+      <p className='text-[#667085] lg:text-xl md:text-base text-sm lg:leading-10 md:leading-6 leading-6'>Subscribe to learn about new product features, the latest in technology, solutions, and updates.</p>
       <form onSubmit={handleSubmit} className='flex flex-col   justify-center items-center'>
         <div className=' flex flex-col '>
           <div className='flex gap-x-4'>
             <input
-              cy-data="newsletter-input"
-              className='py-3 px-3 lg:h-[48px] lg:w-[360px] md:h-[40px] md:w-[300px] h-[35px] w-[250px] border border-[#D0D5DD] rounded-md focus:outline-[#7F56D9]'
+              cy-data='newsletter-input'
+              className='py-3 px-3 lg:h-[48px] lg:w-[360px] md:h-[40px] md:w-[300px] h-[35px] w-[200px] border text-gray-500 border-[#D0D5DD] rounded-md focus:outline-[#7F56D9]'
               type='text'
               name='email'
               value={email}
               placeholder='Email'
               onChange={onChange}
             />
-            <button cy-data="newsletter-button" type='submit' className='l:py-3 lg:px-6 md:py-2 md:px-4 py-1 px-2 text-sm lg:text-xl md:text-sm  bg-[#7F56D9] text-white rounded-md'>
+            <button cy-data='newsletter-button' type='submit' className='l:py-3 lg:px-6 md:py-2 md:px-4 py-1 px-2 text-sm lg:text-xl md:text-sm  bg-[#7F56D9] text-white rounded-md'>
               Subscribe
             </button>
           </div>
